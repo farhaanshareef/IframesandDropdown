01225a061d35fce5a2d775bf6754385960a28817
Farhan Shareef <43530414+farhaanshareef@users.noreply.github.com>
2023-06-23 15:24:09 +0500

    Update IFrames.py

--- IFrames.py
+++ IFrames.py
@@ -4,20 +4,30 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-s= Service("//Users//mac//Downloads//chromedriver_mac_arm64")
+class IFrame:
+    def __init__(self, driver):
+        self.driver = driver
 
-# create webdriver object
-driver = webdriver.Chrome(service=s)
+    def switch_frame(self):
+        s = Service("//Users//mac//Downloads//chromedriver_mac_arm64")
+        driver = webdriver.Chrome(service=s)
 
-# Open the website and maximize the window
-driver.get("https://www.makemytrip.com/")
-driver.maximize_window()
+        # Open the website and maximize the window
+        driver.get("https://www.zameen.com/")
+        driver.maximize_window()
 
-# Switch to the notification frame and close the notification
-frame = WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it("notification-frame-173061603"))
+        # Define the frame and close button locators
+        frame_locator = "google_ads_iframe_/31946216/Splash_660x500_0"
+        close_button_locator = (By.CLASS_NAME, "close_cross_big")
 
-cross_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "(//a[@id='webklipper-publisher-widget-container-notification-close-div'])[1]")))
-cross_button.click()
+        # Switch to the frame
+        iframe = WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(frame_locator))
 
-#switch back to default content
-driver.switch_to.default_content()
+        # Click on the cross icon and close the frame
+        close_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(close_button_locator)).click()
+
+        # Switch back to default content
+        driver.switch_to.default_content()
+
+iframe= IFrame(webdriver)
+iframe.switch_frame()
